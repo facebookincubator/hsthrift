0b313deb0ea4c5562baa07a008db879c25d2f8d2
Simon Marlow <smarlow@fb.com>
2020-12-04 03:11:17 -0800

    Improve install_deps.sh
    Summary:
    Allows for using this script to install dependencies locally, also
    common up boilerplate.
    
    Reviewed By: katiejots
    
    Differential Revision: D25194616
    
    fbshipit-source-id: 95d0654e4ccc5d36ec1de6e18d9ae88bf731f2ef

--- install_deps.sh
+++ install_deps.sh
@@ -1,59 +1,76 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+# Download, build and install dependencies for hsthrift
+#
+# Flags:
+#   --sudo       Use 'sudo make install' (necessary unless you're root)
+#   --nuke       Remove each project after building & installing it
+#   --clean      Remove old build artifacts before building
+#   --threads N  Pass -jN to make
 
 set -e
 
-git clone https://github.com/facebook/folly.git
-cd folly
-# PROJ_TAG="v2020.07.13.00"
-PROJ_TAG=$(git tag | sort -r | head -1)
+NUKE=NO
+SUDO=
+CLEAN=NO
+THREADS=2
+BUILD_SUBDIR=_build
+
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        --nuke) NUKE=YES;;
+        --sudo) SUDO=sudo;;
+        --clean) CLEAN=YES;;
+        --threads) THREADS="$2"; shift;;
+        *)
+            echo "syntax: install_deps.sh [--nuke] [--sudo] [--clean] [--threads N]"
+            exit 1;;
+    esac
+    shift
+done
+
+clone() {
+    dir="$1"
+    url="$2"
+    rev="$3"
+    if [ -d "$dir" ]; then
+        (cd "$dir" && git fetch)
+    else
+        git clone "$url" "$dir"
+    fi
+    if [ "$rev" != "" ]; then
+        (cd "$dir" && git checkout "$rev")
+    fi
+}
+
+build() {
+    dir="$1"
+    subdir="$2"
+    cd "$dir"
+    [ "$CLEAN" = "NO" ] || rm -rf "$BUILD_SUBDIR"
+    mkdir -p "$BUILD_SUBDIR"
+    cd "$BUILD_SUBDIR"
+    # cmake -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE -DBUILD_SHARED_LIBS=ON -DBUILD_EXAMPLES=off -DBUILD_TESTS=off ..
+    cmake -DBUILD_SHARED_LIBS=ON -DBUILD_EXAMPLES=off -DBUILD_TESTS=off ../"$subdir"
+    make -j"$THREADS" && $SUDO make install
+    cd ../..
+    [ "$NUKE" = "NO" ] || rm -rf "$dir"
+}
+
+clone folly https://github.com/facebook/folly.git
+PROJ_TAG=$(cd folly && git tag | sort -r | head -1)
 echo "Using tag: $PROJ_TAG"
-git checkout $PROJ_TAG
-# curl -s -o simonspatch.diff https://github.com/simonmar/folly/commit/9b1e7eaf2513ecd51e42336f950b4b79d816ac95.diff && git apply simonspatch.diff
-mkdir _build
-cd _build
-# cmake -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE -DBUILD_SHARED_LIBS=ON -DBUILD_EXAMPLES=off -DBUILD_TESTS=off ..
-cmake -DBUILD_SHARED_LIBS=ON -DBUILD_EXAMPLES=off -DBUILD_TESTS=off ..
-make -j2 && make install
-cd ../..
-rm -rf folly/
+(cd folly && git checkout "$PROJ_TAG")
+build folly .
 
-git clone https://github.com/rsocket/rsocket-cpp.git
-cd rsocket-cpp
-git checkout $PROJ_TAG
-mkdir _build
-cd _build
-# cmake -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE -DBUILD_SHARED_LIBS=ON -DBUILD_EXAMPLES=off -DBUILD_TESTS=off ..
-cmake -DBUILD_SHARED_LIBS=ON -DBUILD_EXAMPLES=off -DBUILD_TESTS=off ..
-make -j2 && make install
-cd ../..
-rm -rf rsocket-cpp/
+clone rsocket-cpp https://github.com/rsocket/rsocket-cpp.git "$PROJ_TAG"
+build rsocket-cpp .
 
-git clone https://github.com/facebookincubator/fizz.git
-cd fizz
-git checkout $PROJ_TAG
-mkdir _build
-cd _build
-cmake -DBUILD_SHARED_LIBS=ON -DBUILD_EXAMPLES=off -DBUILD_TESTS=off ../fizz
-make -j2 && make install
-cd ../..
-rm -rf fizz/
+clone fizz https://github.com/facebookincubator/fizz.git "$PROJ_TAG"
+build fizz fizz
 
-git clone https://github.com/facebook/wangle.git
-cd wangle
-git checkout $PROJ_TAG
-mkdir _build
-cd _build
-cmake -DBUILD_SHARED_LIBS=ON -DBUILD_EXAMPLES=off -DBUILD_TESTS=off ../wangle
-make -j2 && make install
-cd ../../
-rm -rf wangle/
+clone wangle https://github.com/facebook/wangle.git "$PROJ_TAG"
+build wangle wangle
 
-git clone https://github.com/facebook/fbthrift.git
-cd fbthrift
-git checkout $PROJ_TAG
-mkdir _build
-cd _build
-cmake -DBUILD_SHARED_LIBS=ON -DBUILD_EXAMPLES=off -DBUILD_TESTS=off ..
-make -j2 && make install
-cd ../..
-rm -rf fbthrift/
+clone fbthrift https://github.com/facebook/fbthrift.git "$PROJ_TAG"
+build fbthrift .
