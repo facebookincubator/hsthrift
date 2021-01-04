7e9afb8f11b376851c436963c2452ec89f247f40
Alp Mestanogullari <alp@well-typed.com>
2021-01-04 15:43:50 +0100

    Support producing proper source distributions
    Where each package can be tested on its own (and therefore embeds all the
    appropriate files), via a source distribution. The Makefile changes
    introduce rules for copying all the right files in the right places.
    
    The compiler's test infrastructure is appropriately updated to accomodate for
    the new scenario, where we can't just hardcode the repository's structure and
    need to be both a bit more careful in how we set things up for the tests, and
    more lenient when it comes to comparing the expected output and what the test
    actually produced, occasionally stripping some directory name from paths.

--- common/util/tests/github/Facebook/Init.hs
+++
@@ -1,7 +0,0 @@
-module Facebook.Init where
-
-withFacebook :: IO () -> IO ()
-withFacebook = id
-
-withFacebookUnitTest :: IO a -> IO a
-withFacebookUnitTest = id
--- lib/test/github/TestRunner.hs
+++
@@ -1,7 +0,0 @@
-module TestRunner (module TestRunner) where
-
-import Test.HUnit
-import Test.HUnit.Text
-
-testRunner :: Test -> IO ()
-testRunner = runTestTTAndExit
