1dd7b0d14af16b357d23eab389edeae4a380b7d0
Pepe Iborra <pepeiborra@fb.com>
2021-06-22 10:47:56 -0700

    fix listing of Haskell files for target generation
    Summary:
    `sigma-builder prepare haxlsh` was looping for me on `~/si_sigma` during target generation. It turned out to be the usual problem - the buck-out folder contains cyclic symlinks that make `listFilesRecursive` loop as warned in the documentation.
    
    I generalised some code from MDO to fix the problem
    
    Reviewed By: rayshih
    
    Differential Revision: D29261588
    
    fbshipit-source-id: 2bd87f5bfcb54973fa1650fdb05a803c346fab59

---
+++ common/util/Util/Directory.hs
@@ -0,0 +1,45 @@
+-- Copyright 2004-present Facebook. All Rights Reserved.
+
+module Util.Directory (
+  Directory(..),
+  listFilesRecursive,
+) where
+
+import Control.Monad.Extra (concatMapM)
+import Control.Monad.State
+import qualified Data.HashSet as Set
+import qualified System.Directory as IO
+import System.FilePath
+
+-- | An effect class for modelling directory I/O
+class Directory f where
+  doesDirectoryExist :: FilePath -> f Bool
+  listDirectory :: FilePath -> f [FilePath]
+
+instance Directory IO where
+  doesDirectoryExist = IO.doesDirectoryExist
+  listDirectory = IO.listDirectory
+
+{- | A version of 'listFilesRecursive' from the 'extra' package offering more
+   control and termination in the presence of cyclic symlinks.
+-}
+listFilesRecursive ::
+  (Directory f, Monad f) =>
+  -- | Whether to recurse in a directory
+  (FilePath -> Bool) ->
+  FilePath ->
+  f [FilePath]
+listFilesRecursive predDir = flip evalStateT mempty . go
+ where
+  go relDir = do
+    visited <- get
+    if Set.member relDir visited
+      then return []
+      else do
+        modify $ Set.insert relDir
+        ls <- lift $ map (relDir </>) <$> listDirectory relDir
+        dirs <- lift $ filterM (\d -> (&& predDir d) <$> doesDirectoryExist d) ls
+        (ls ++) <$> concatMapM go dirs
+
+{-# SPECIALIZE
+    listFilesRecursive :: (FilePath -> Bool) -> FilePath -> IO [FilePath] #-}
