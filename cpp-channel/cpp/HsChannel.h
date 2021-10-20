f1b7c4e189d37a21a28c01dfd5623189f629e0ce
Sebastian Lund <selund@fb.com>
2021-10-20 05:51:28 -0700

    Move ChannelWrapper::sendRequest* into cpp-file
    Summary:
    Tidying up by moving the sendRequest functions into the cpp-file
    rather than defining them in the header.
    
    Reviewed By: simonmar
    
    Differential Revision: D31790275
    
    fbshipit-source-id: c0f91375a13c7b23ea03806ccdf67a58d9dea9c2

--- cpp-channel/cpp/HsChannel.h
+++ cpp-channel/cpp/HsChannel.h
@@ -202,51 +202,7 @@ class ChannelWrapper {
       HsStablePtr recv_mvar,
       FinishedRequest* send_result,
       FinishedRequest* recv_result,
-      apache::thrift::RpcOptions&& rpcOpts) {
-    auto msg = folly::IOBuf::wrapBuffer(buf, len);
-    using CallbackPtr = std::unique_ptr<
-        HsCallback,
-        apache::thrift::RequestClientCallback::RequestClientCallbackDeleter>;
-    auto cob = CallbackPtr(new HsCallback(
-        client_, capability, send_mvar, recv_mvar, send_result, recv_result));
-
-    auto sendRequestImpl = [protId = getProtocolType(buf[0]),
-                            client = client_,
-                            cob = std::move(cob),
-                            msg = std::move(msg),
-                            rpcOpts = std::move(rpcOpts)]() mutable {
-      auto header = std::make_shared<apache::thrift::transport::THeader>(0);
-      header->setProtocolId(protId);
-      for (auto const& rpcHeader : rpcOpts.getWriteHeaders()) {
-        header->setHeader(rpcHeader.first, rpcHeader.second);
-      }
-
-      if (auto envelopeAndRequest =
-              apache::thrift::EnvelopeUtil::stripRequestEnvelope(
-                  std::move(msg))) {
-        cob->setMethodName(envelopeAndRequest->first.methodName);
-        client->get()->sendRequestResponse(
-            rpcOpts,
-            envelopeAndRequest->first.methodName,
-            apache::thrift::SerializedRequest(
-                std::move(envelopeAndRequest->second)),
-            header,
-            std::move(cob));
-      } else {
-        cob.release()->onResponseError(
-            folly::make_exception_wrapper<
-                apache::thrift::transport::TTransportException>(
-                apache::thrift::transport::TTransportException::CORRUPTED_DATA,
-                "Unexpected problem stripping envelope"));
-      }
-    };
-
-    if (auto evb = client_->get()->getEventBase()) {
-      evb->add(std::move(sendRequestImpl));
-    } else {
-      sendRequestImpl();
-    }
-  }
+      apache::thrift::RpcOptions&& rpcOpts);
 
   void sendOnewayRequest(
       uint8_t* buf,
@@ -254,46 +210,7 @@ class ChannelWrapper {
       int capability,
       HsStablePtr send_mvar,
       FinishedRequest* send_result,
-      apache::thrift::RpcOptions&& rpcOpts) {
-    auto msg = folly::IOBuf::wrapBuffer(buf, len);
-    auto cob = apache::thrift::RequestClientCallback::Ptr(new HsCallback(
-        client_, capability, send_mvar, nullptr, send_result, nullptr));
-
-    auto sendOnewayRequestImpl = [protId = getProtocolType(buf[0]),
-                                  client = client_,
-                                  cob = std::move(cob),
-                                  msg = std::move(msg),
-                                  rpcOpts = std::move(rpcOpts)]() mutable {
-      auto header = std::make_shared<apache::thrift::transport::THeader>(0);
-      header->setProtocolId(protId);
-      for (auto const& rpcHeader : rpcOpts.getWriteHeaders()) {
-        header->setHeader(rpcHeader.first, rpcHeader.second);
-      }
-
-      if (auto envelopeAndRequest =
-              apache::thrift::EnvelopeUtil::stripRequestEnvelope(
-                  std::move(msg))) {
-        client->get()->sendRequestNoResponse(
-            rpcOpts,
-            envelopeAndRequest->first.methodName,
-            apache::thrift::SerializedRequest(
-                std::move(envelopeAndRequest->second)),
-            header,
-            std::move(cob));
-      } else {
-        cob.release()->onResponseError(
-            folly::make_exception_wrapper<
-                apache::thrift::transport::TTransportException>(
-                apache::thrift::transport::TTransportException::CORRUPTED_DATA,
-                "Unexpected problem stripping envelope"));
-      }
-    };
-    if (auto evb = client_->get()->getEventBase()) {
-      evb->add(std::move(sendOnewayRequestImpl));
-    } else {
-      sendOnewayRequestImpl();
-    }
-  }
+      apache::thrift::RpcOptions&& rpcOpts);
 
   InnerChannel* getInnerRequestChannel() {
     return client_.get();
