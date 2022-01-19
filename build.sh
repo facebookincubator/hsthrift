16277f3dffe2789c3de35da77a12dd1691f3a12a
Don Stewart <don.stewart@tngtech.com>
2022-01-19 05:09:00 +0000

    Make sure to turn on intrinsics on x86 for folly in getdeps.py
    Not sure if this is the right way to do it, but mirrors what we do in
    the install_deps.sh. On x86 we need intrinsics support (corei7 flavored
    folly). Otherwise you'll get a folly misisng symbol error about F14.

--- build.sh
+++ build.sh
@@ -6,6 +6,17 @@ GETDEPS_PATHS=(
     "$SCRIPT_DIR/../../../opensource/fbcode_builder/getdeps.py"
 )
 
+# folly requires certain architecture flags to be used consistently
+# when compiling the library and the application code. Getting this
+# wrong results in a link error (fortunately), see
+#   folly/container/detail/F14IntrinsicsAvailability.h
+# In Glean we use some AVX2 intrinsics, so we have to pick a compatible
+# architecture here for compiling folly.
+arch=$(uname -m)
+if [ "$arch" == x86_64 ] ; then
+    export CXXFLAGS=-march=corei7
+fi
+
 # N.B. we always need shared libs, but this only checks build is the first arg
 if [ "$1" = "build" ]; then
     set -- "$@" --extra-cmake-defines='{"BUILD_SHARED_LIBS": "ON", "BUILD_EXAMPLES": "off", "BUILD_TESTS": "off", "CMAKE_INSTALL_RPATH_USE_LINK_PATH": "TRUE", "EVENT__BUILD_SHARED_LIBRARIES": "ON"}'
