415b6d3a9e0486e6d9434795ca35420da83bb2ad
Don Stewart <don.stewart@tngtech.com>
2022-01-31 09:01:13 -0800

    Add support for installing additional packages from cmd line args (#61)
    Summary:
    This extends the deps installer with enough smarts to install additional packages on the command line.
    We do this so that Glean can ask the hsthrift build_deps.py to also solve the rocksdb source dependency, which is now required.
    
    No changes to hsthrift itself, but this will unblock glean. Updates the rocksdb manifest we use to point at git.
    
    Pull Request resolved: https://github.com/facebookincubator/hsthrift/pull/61
    
    Reviewed By: nhawkes
    
    Differential Revision: D33889630
    
    Pulled By: simonmar
    
    fbshipit-source-id: 3050f2c205b454168970b08101314c938f5d995f

--- new_install_deps.sh
+++ new_install_deps.sh
@@ -2,30 +2,33 @@
 
 # Copyright (c) Facebook, Inc. and its affiliates.
 
-#
-# These are source deps not satisifiable with common package systems
-# in topological order
-#
-DEPS="folly fizz wangle fbthrift"
-
 set -e
 
 BUILDER=./build.sh
-THREADS=2
+THREADS=4
+
+#
+# These are required source deps not satisifiable with common package systems
+# in topological order. You can pass additional deps on the command line.
+#
+DEPS="folly fizz wangle fbthrift"
+
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        --threads) THREADS="$2"; shift; shift;;
+        *) DEPS+=" $1"; shift;;
+    esac
+done
 
 # default library and bin install path
 if [ -z "${INSTALL_PREFIX}" ]; then
     INSTALL_PREFIX="${HOME}/.hsthrift"
 fi
 
-build() {
-    ${BUILDER} build --no-deps --install-dir="${INSTALL_PREFIX}" \
-        --num-jobs="${THREADS}" "$1"
-}
-
 # build in order
 for dep in $DEPS; do
-    build "$dep"
+    ${BUILDER} build --no-deps --install-dir "${INSTALL_PREFIX}" \
+        --num-jobs "${THREADS}" "$dep"
 done
 
 # add these to your environment
