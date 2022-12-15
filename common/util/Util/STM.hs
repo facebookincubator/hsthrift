5ef7f71116f39282b4e56c46a27170d80b395564
Pepe Iborra <pepeiborra@meta.com>
2022-12-14 22:38:58 -0800

    Labelled STM transactions
    Summary: This uses implicit call stacks to label STM deadlock exceptions. I took this trick from the `stm-stats` package, which also a few more nifty tricks.
    
    Reviewed By: bochko, lazamar
    
    Differential Revision: D42038900
    
    fbshipit-source-id: e151750037b7eacb222eb384821fd7f50111c332

--- common/util/Util/STM.hs
+++ common/util/Util/STM.hs
@@ -2,9 +2,14 @@
 
 module Util.STM
  ( updateTVar
+ , atomicallyWithLabel
+ , Util.STM.atomically
  ) where
 
-import Control.Concurrent.STM
+import Control.Concurrent.STM as STM
+import Control.Exception (Exception, BlockedIndefinitelyOnSTM, catch, throwIO)
+import GHC.Stack
+import Data.Typeable (Typeable)
 
 -- | version of @modifyTVar'@ that returns the new value
 updateTVar :: TVar a -> (a -> a) -> STM a
@@ -13,3 +18,35 @@ updateTVar var f = do
   let !x' = f x
   writeTVar var x'
   return x'
+
+-- | Version of 'atomically' that takes a label
+atomicallyWithLabel :: String -> STM a -> IO a
+atomicallyWithLabel label stm = do
+  STM.atomically stm `catch` (\(_::BlockedIndefinitelyOnSTM) ->
+    throwIO (BlockedIndefinitelyOnNamedSTM label))
+
+-- | Version of 'atomically' that labels the transaction using the call site
+atomically :: HasCallStack => STM a -> IO a
+atomically =
+  atomicallyWithLabel (getCaller $ getCallStack callStack)
+
+-- | The calling point is actually right next to the head as the head is the
+-- current function (in this case it would be logInfo/Warning/Error/Fatal).
+getCaller :: [(String, SrcLoc)] -> String
+getCaller cs =
+  case cs of
+    -- Take the first element of the stack if it exists
+    ((_,sl):_) -> srcLocFile sl
+    _       -> "Unknown stack trace"
+
+-- | 'atomicallyWithLabel' replaces occurrences of 'BlockedIndefinitelyOnSTM' w/
+-- 'BlockedIndefinitelyOnNamedSTM', carrying the name of the transaction and
+-- thus giving more helpful error messages.
+newtype BlockedIndefinitelyOnNamedSTM = BlockedIndefinitelyOnNamedSTM String
+    deriving (Typeable)
+
+instance Show BlockedIndefinitelyOnNamedSTM where
+    showsPrec _ (BlockedIndefinitelyOnNamedSTM name) =
+        showString $ "thread blocked indefinitely in STM transaction" ++ name
+
+instance Exception BlockedIndefinitelyOnNamedSTM
