e419d4c35137a7201e2abd6020edde78e0cd196d
Myungho Jung <myjung@meta.com>
2026-05-09 18:11:48 -0700

    Expand php_hack_ns fixture coverage
    Summary: Expand the php_hack_ns test fixture to improve coverage of Hack codegen features like `NamePrefix`, `ConstantsClass`, and `typedef` mode. Adds a service method (`ping`), new Thrift definitions (typedef, union, exception, struct with `edward-io.NamePrefix`), a constants class annotation, and a new `hack_typedef` output variant to validate that these features interact correctly with PHP/Hack namespace handling.
    
    Reviewed By: rmakheja
    
    Differential Revision: D101434627
    
    fbshipit-source-id: 8993e26a56d231c4e35f4a8f87311b41893d74d4

--- exactprint/tests/fbthrift-tests/php_hack_ns/src/module.thrift
+++ exactprint/tests/fbthrift-tests/php_hack_ns/src/module.thrift
@@ -18,17 +18,43 @@
  * limitations under the License.
  */
 
+include "thrift/annotation/hack.thrift"
+
+@hack.ConstantsClass{name = "AnnotatedPhpConstants"}
+@hack.NamePrefix{prefix = "ProgramPrefixed"}
 package "test.dev/foo/php/ns"
 
 namespace php foo.php.ns
+
+const i32 ANSWER = 42;
+
+@hack.NamePrefix{prefix = "Prefixed"}
+typedef string TestTypedef
 
 enum Status {
   Unknown = 0,
 }
 
-struct TestStruct {
-  1: string str_value;
+@hack.NamePrefix{prefix = "Prefixed"}
+union TestUnion {
+  1: string string_value;
 }
 
+@hack.NamePrefix{prefix = "Prefixed", apply_on_getName = false}
+exception TestException {
+  1: string message;
+}
+
+@hack.NamePrefix{prefix = "Double_", apply_on_getName = false}
+@hack.Name{name = "Prefixed_"}
+struct TestStruct {
+  1: string str_value;
+  2: TestTypedef typedef_value;
+  3: TestUnion union_value;
+}
+
+@hack.NamePrefix{prefix = "Deprecated_"}
 service FooHackService {
+  Status fetchStatus(1: TestStruct request);
+  TestStruct ping(1: string str_arg) throws (1: TestException ex);
 }
